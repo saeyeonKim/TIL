e6f28fcc0c1200cbe67dcbf435606254b6f96cbb
saeyeonKim <118176015+saeyeonKim@users.noreply.github.com>
2022-12-05 22:42:30 +0900

    until1-6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 // src/App.js
 
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
+import { useSelector, useDispatch } from "react-redux"; // import 해주세요.
 
 const App = () => {
-  const counterStore = useSelector((state) => state);
-  // 위의 뜻은 const counterStore = useSelector(function(state){return state;}) 와 같은뜻
+  // const counterStore = useSelector((state) => state);
+  // // 위의 뜻은 const counterStore = useSelector(function(state){return state;}) 와 같은뜻
+  // // 화살표 함수에서 꺼낸 state 인자는 현재 프로젝트에서 존재하는 모든 redux module의 state를 가져와라 라는 뜻!
+  // const number = useSelector((state) => state.counter.number);
+  // // state 안에 counter 안에 있는 number를 가져와라 라는 뜻!
+  // console.log(counterStore); // 값: number:0
+  // console.log(number); // 값 : 0
+
+  const dispatch = useDispatch(); // dispatch는 함수여서 꼭 괄호 붙여야된다. 괄호 안에 action 객체를 넣음.
   const number = useSelector((state) => state.counter.number);
-  console.log(counterStore);
   console.log(number);
-  return <div></div>;
+  return (
+    <div>
+      {number}
+      <button
+        onClick={() => {
+          dispatch({ type: "PLUS_ONE" });
+          // onclick을 했을 때 dispatch를 통해서 PLUS_ONE이라는 action을 실행하라는 뜻.
+          // dispatch를 통해서 action 객체를 reducer로 보낼 수 있다.
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "MINUS_ONE" });
+        }}
+      >
+        -1
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,13 +1,18 @@
 // src/modules/counter.js
 
-// 초기 상태값
+// 초기 상태값(useState의 초기값설정과 같은 역할)
 const initialState = {
-  number: 0,
+  number: 0, // 객체 안에 number에 0이라는 초기값을 설정한다, 객체뿐만 아니라 배열이나 원시데이터, 0,1,2 이런식으로도 가능
 };
 
 // 리듀서 : 변화를 일으키는 함수(useState의 set함수 같은 역할)
 const counter = (state = initialState, action) => {
+  console.log(action);
   switch (action.type) {
+    case "PLUS_ONE":
+      return { number: state.number + 1 }; //number에 state에 있는 number+1을 넣겠다.
+    case "MINUS_ONE":
+      return { number: state.number - 1 };
     default:
       return state;
   }
